80625e5d773f03415390fc68c146820bd40a33bd
Tezan <tezansahu@gmail.com>
2020-03-26 04:37:51 +0530

    volunteer matching done

--- client/js/firebase.js
+++ client/js/firebase.js
@@ -1,5 +1,7 @@
 let url = "http://127.0.0.1:3000";
 
+let requests = {};
+
 var firebaseConfig = {
     apiKey: "AIzaSyAao3z-m_bfaVk6LdAKn1CmOMkMmvFSFZk",
     authDomain: "helpinghand-tsrn.firebaseapp.com",
@@ -108,16 +110,40 @@ function retrieveCauses(){
     })
 }
 
+
 function retrieveAidsNeeded(){
-    let service_type;
-    let database = firebase.database().ref('services/' + service_type);
-    database.on('value', function(snapshot){
-        snapshot.forEach(snap => {
-        
+    let service_types = ["medicines", "daily_essentials", "physical_assistance"];
+
+    for(i =0; i < service_types.length; i++){
+        let service_type = service_types[i];
+        let database = firebase.database().ref('services/' + service_type);
+        database.on('value', function(snapshot){
+            snapshot.forEach(snap => {
+                console.log(snap.val())
+                console.log(`${service_type}_table_body`)
+                document.getElementById(`${service_type}_table_body`).innerHTML += 
+                    `<tr class="clickable-row" value=${snap.key}>
+                    <th scope="row">${snap.key}</th>
+                    <td>${snap.val()["name"]}</td>
+                    <td>${snap.val()["res_addr"]}</td>
+                    <td>${snap.val()["descr"]}</td>
+                    <td>${snap.val()["status"]}</td>
+                    </tr>
+                    `
+                requests[snap.key] = {
+                    "name": snap.val()["name"],
+                    "addr": snap.val()["res_addr"],
+                    "descr": snap.val()["descr"],
+                    "status": snap.val()["status"],
+                    "type": service_type
+                }
+            })
         })
-    })
+    }
+    
 }
 
+retrieveAidsNeeded()
 
 function newAidNeeded(){
     let name = document.getElementById("service_name").value;
@@ -134,24 +160,58 @@ function newAidNeeded(){
         descr: descr,
         status: "Help Needed",
     });
+
+    alert("Request for Assistance has been recorded")
 }
 
 function matchAidWithVolunteer(){
-    let id;
+    let id = document.getElementById("volunteer_id").value;
     let service_type;
-    let volunteer;
-    let database = firebase.database().ref('services/' + service_type);
+    try{
+        service_type = requests[id]["type"];
+    }
+    catch {
+        alert("Invalid ID");
+        return
+    }
+    
+    let volunteer = "Mr. X";
+    try{
+        let database = firebase.database().ref('services/' + service_type);
         database.on('value', function(snapshot){
             snapshot.forEach(snap => {
-                if(JSON.parse(snap.val().id) == id) {
-                    new_withdrawn = snap.val().donated;
-                    let db = firebase.database().ref('services/' + service_type + snap.key);
+                if(snap.key == id) {
+                    alert("Found")
+                    let db = firebase.database().ref('services/' + service_type + "/" + snap.key);
                     db.update({
                         status: "Volunteer: " + volunteer
                     })
                 }
             })
         })
+        
+    }
+    catch{
+        alert("Sorry! Could not process your request")
+        return;
+    }
+    alert("You have been successfully matched to " + requests[id]["name"])
+    
+}
+
+function getUserDetails(){
+    let database = firebase.database().ref('users');
+    database.on('value', function(snapshot){
+        snapshot.forEach(snap => {
+            if(JSON.parse(snap.val().id) == id) {
+                new_withdrawn = snap.val().donated;
+                let db = firebase.database().ref('services/' + service_type + snap.key);
+                db.update({
+                    status: "Volunteer: " + volunteer
+                })
+            }
+        })
+    })
 }
 
 
